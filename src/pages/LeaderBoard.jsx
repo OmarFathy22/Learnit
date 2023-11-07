2031aed35d0b7d4e5f8d981e6dae889e814424c5
Omar Fathy <omar.fathy.web@gmai.com>
2023-11-07 07:07:50 +0200

    new

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -51,7 +51,7 @@ const Root = (props) => {
             mode={mode}
             setmyMode={setmyMode}
           />
-          <div className="border-[1px] sm:mt-[120px] border-gray-200 mt-[70px] w-full mx-7 rounded-md ">
+          <div className="border-[1px] sm:yt-[120px] border-gray-200 my-[70px] w-full mx-7 rounded-md ">
             <div>
             <div  className={`m-2 px-2 py-1 rounded-md flex justify-between items-center sm:pr-[50px] pr-[100px] bg-[#dcd9d9]`} >
                     <div className="flex gap-2">
