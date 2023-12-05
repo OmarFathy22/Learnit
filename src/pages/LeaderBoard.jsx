abcf4c5037878b97cefe1dad826a0fe834814231
Omar Fathy <omar.fathy.web@gmail.com>
2023-12-05 04:40:18 +0200

    new

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -7,17 +7,22 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import Appbar from "../components/Appbar";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Outlet } from "react-router";
 import getDesignTokens from "../styles/MyTheme";
 import DRAWER from "../components/DRAWER";
 import { data3 } from "../../Data";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/config";
+
+
 const Root = (props) => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
   const [showList, setshowList] = useState("none");
+  const [Users, setUsers] = useState([]);
   const [mode, setmyMode] = useState(
     localStorage.getItem("currentMode") === null
       ? "dark"
@@ -25,6 +30,38 @@ const Root = (props) => {
       ? "light"
       : "dark"
   );
+  useEffect(() => {
+    const getAllDocuments = async (collectionName) => {
+      try {
+        const collectionRef = collection(db, collectionName);
+        const querySnapshot = await getDocs(collectionRef);
+    
+        const documents = [];
+        querySnapshot.forEach((doc) => {
+          documents.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+    
+        return documents;
+      } catch (error) {
+        console.error("Error getting documents: ", error);
+        throw error;
+      }
+    };
+    
+    // Example usage
+    const collectionName = "Users";
+    getAllDocuments(collectionName)
+      .then((documents) => {
+        console.log("Documents: ", documents);
+        setUsers(documents);
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+  }, []);
   const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   return (
     <ThemeProvider theme={theme}>
@@ -51,7 +88,7 @@ const Root = (props) => {
             mode={mode}
             setmyMode={setmyMode}
           />
-          <div className="border-[1px] max-600:mt-[120px] border-gray-200 my-[100px] pb-2 w-full mx-7 rounded-md ">
+          <div className="border-[1px] max-600:mt-[120px] border-gray-200 my-[100px] pb-2 w-full mx-7 rounded-md h-[80vh] overflow-auto ">
             <div>
             <div  className={`m-2 px-2 py-1 rounded-md flex justify-between items-center sm:pr-[50px] pr-[100px] bg-[#dcd9d9]`} >
                     <div className="flex gap-2">
@@ -61,18 +98,18 @@ const Root = (props) => {
                       points
                     </div>
                   </div>
-              {data3.map((item , index) => {
+              {Users.map((user , index) => {
                 return(
                   <div key={index} className={`mx-2 px-2 py-1 rounded-md flex justify-between items-center ${index %2 == 0 && "bg-[#dcd9d9]"}`} >
                     <div className="flex gap-2">
-                      <div><img className="w-10 h-10 rounded-full" src={item.src} alt="" /></div>
+                      <div><img className="w-10 h-10 rounded-full" src={user?.photoUrl} alt="" /></div>
                       <div>
-                        <div>{item.name}</div>
-                        <div>level {item.level}</div>
+                        <div>{user?.username}</div>
+                        <div>level {user?.level}</div>
                       </div>
                     </div>
                     <div className="bg-[#7ee97eac] text-[#266e26] px-5 py-[1px] rounded-full sm:mr-[30px] mr-[80px]">
-                      {item.points}
+                      {user?.points}
                     </div>
                   </div>
                 )
