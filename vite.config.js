1d2c4399938f87b70e5bdb00986f54130bfaa3af
Omar Fathy <omar.fathy.web@gmail.com>
2023-12-05 07:48:03 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,45 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const minfest = {
-  display: "standalone",
-  orientation: "portrait",
-  background_color: "#ffffff",
-  theme_color: "#ffffff",
-
-  scope: "/",
-  start_url: "/",
-  name: "TASKATI",
-  short_name: "TASKATI",
-  icons: [
-    {
-      src: "/icon-192x192.png",
-      sizes: "192x192",
-      type: "image/png",
-    },
-    {
-      src: "/icon-256x256.png",
-      sizes: "256x256",
-      type: "image/png",
-    },
-    {
-      src: "/icon-384x384.png",
-      sizes: "384x384",
-      type: "image/png",
-    },
-    {
-      src: "/icon-512x512.png",
-      sizes: "512x512",
-      type: "image/png",
-    },
-  ],
-};
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+      },
+      {
+        src: "/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(minfest)],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 });
