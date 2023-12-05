69244296aa1698eced9085130c2870593e7d0a24
Omar Fathy <omar.fathy.web@gmail.com>
2023-12-05 07:07:13 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const minfest = {
+  display: "standalone",
+  orientation: "portrait",
+  background_color: "#ffffff",
+  theme_color: "#ffffff",
+
+  scope: "/",
+  start_url: "/",
+  name: "TASKATI",
+  short_name: "TASKATI",
+  icons: [
+    {
+      src: "/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+    {
+      src: "/icon-384x384.png",
+      sizes: "384x384",
+      type: "image/png",
+    },
+    {
+      src: "/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(minfest)],
+});
